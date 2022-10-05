c32add8d3a18de034a55e5f8df7b5465072535d8
zahra987 <zhr.ramezanpour@gmail.com>
2022-10-05 23:08:19 +0330

    b

--- src/Components/Pages/Saved.jsx
+++ src/Components/Pages/Saved.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Card from '../Card';
 import '../../Css/saved.css';
 export default function Saved() {
   return (
-   <div className='saved_container' >
-    <Card/>
+   <div >
+    Saved
    </div>
   )
 }
